f5aa5224f3fb0194873cc76be1ba45e78c89fe29
Victor <victorsetlhare@gmail.com>
2020-04-25 16:51:41 +0100

    ebike class passed

--- ebike.rb
+++ ebike.rb
@@ -1,11 +1,10 @@
 class Ebike 
 
-    attr_reader :name, :brand, :quantity, :retail_price, :cost
+    attr_reader :name, :brand, :retail_price, :cost
 
-    def initialize(name, brand, quantity, retail_price, cost)
+    def initialize(name, brand, retail_price, cost)
         @name = name
         @brand = brand
-        @quantity = quantity
         @retail_price = retail_price
         @cost = cost
     end 
--- spec/ebike_spec.rb
+++ spec/ebike_spec.rb
@@ -6,7 +6,7 @@ require_relative("../ebike")
 
 class TestPet < Minitest::Test
     def setup
-        @ebike1 = Ebike.new("Tern GSD", "Tern", 5, 3995, 3295 )
+        @ebike1 = Ebike.new("Tern GSD", "Tern", 3995, 3295 )
     end
 
     def test_ebike_has_name()
@@ -15,6 +15,14 @@ class TestPet < Minitest::Test
 
     def test_ebike_has_brand()
         assert_equal("Tern", @ebike1.brand)
+    end
+
+    def test_ebike_has_retail_price
+        assert_equal(3995, @ebike1.retail_price)
+    end
+
+    def test_ebike_has_cost
+        assert_equal(3295, @ebike1.cost)
     end
 
 end
